69381f5406abee32f14d030a56927dd6a2eb5143
codingpersonal <codingpersonal@gmail.com>
2015-11-10 12:29:57 -0800

    Adding anagrams project

--- PermString/src/StringPerm.java
+++ PermString/src/StringPerm.java
@@ -2,48 +2,34 @@ public class StringPerm {
 
 	public static void main(String[] args) {
 		String input = "somya";
+		char ip[] = input.toCharArray();
+		printPermutations(ip, 0, input.length());
+		System.out.println("perms are: " +  cc);
+	}
 		
-		int len = input.length();
-		for(int i=0; i < len; i++) {
-			String swapInput = swap(input,i);
-			System.out.println(swapInput);
-			System.out.println(swapInput.charAt(0)+" "+swapInput.substring(1, len));
-		}
+	static void printString(char[]c , int size) {
+		System.out.println(String.valueOf(c));
 	}
+	static int cc = 0;
 	
-	static String swap(String inp, int index) {
-		char[] ch = inp.toCharArray();
-		char temp = ch[0];
-		ch[0] = ch[index];
-		ch[index] = temp;
-		String swappedString = new String(ch);
-		return swappedString;
-	}
-	
-	static String printPermutations(char c, String perm) {
-		if(perm.length() <= 1) {
-			return null;
-		}
-		else if(perm.length() == 2) {
-			//swap both these
-			char[] ch = perm.toCharArray();
-
-			// Replace with a "swap" function, if desired:
-			char temp = ch[0];
-			ch[0] = ch[1];
-			ch[1] = temp;
-
-			String swappedString = new String(ch);
-			perm = swappedString;
-			return perm;
-			
+	static void printPermutations(char[] c, int start, int inputSize) {
+		if(start == inputSize -1) {
+			cc++;
+			printString(c, inputSize);
 		}
 		else {
-			char fix = perm.charAt(0);
-			System.out.println(fix+""+printPermutations(fix, perm.substring(1,perm.length())));
-			return null;
-			
+			for(int i = start; i < inputSize; i++) {
+				
+				char temp = c[start];
+				c[start] = c[i];
+				c[i] = temp;
+
+				printPermutations(c, start + 1, inputSize);
+
+				temp = c[start];
+				c[start] = c[i];
+				c[i] = temp;
+			}
 		}
 	}
-
 }
